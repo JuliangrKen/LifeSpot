c6dd4a66192229a17f7b527122543c5445db9d6d
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2022-11-30 20:13:47 +0300

    29.3.8

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,24 +1,22 @@
-﻿let sessionHandler = function handleSession() {
-    // создадим объект Map для хранения сессии
+﻿function handleSession() {
     let session = new Map();
-    // Сохраним UserAgent
     session.set("userAgent", window.navigator.userAgent)
-
-    // Запросим возраст пользователя и тоже сохраним
     session.set("age", prompt("Пожалуйста, введите ваш возраст"))
 
-    // Проверка на возраст и сохранение сессии
     if (session.get("age") >= 18) {
         let startDate = new Date().toLocaleString();
 
         alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + startDate);
         session.set("startDate", startDate)
-    }
-    else {
+    } else {
         alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
         window.location.href = "http://www.google.com"
     }
-    // Вывод в консоль
+
+    return session
+}
+
+function printSession(session) {
     for (let result of session) {
         console.log(result)
     }
