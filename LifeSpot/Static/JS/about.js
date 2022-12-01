b9eefeb22ddc4d35edf7b6a76f47dd1a45b1a1fa
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2022-12-01 19:11:10 +0300

    29.7.5

---
+++ LifeSpot/Static/JS/about.js
@@ -0,0 +1,22 @@
+﻿function sendReview() {
+    let review = {}
+
+    review.username = prompt("Введите ваше имя")
+
+    if (review.username == null) return
+
+    review.text = prompt("Введите ваш текст")
+
+    if (review.text == null) return
+
+    review.date = new Date().toLocaleString()
+
+    addNewReview(review)
+}
+
+let addNewReview = review => {
+   document.getElementsByClassName('reviews')[0].innerHTML += '    <div class="review-text">\n' +
+       `<p> <i> <b>${review.username}</b>  ${review.date}</i></p>` +
+       `<p>${review.text}</p>`  +
+       '</div>';
+}
