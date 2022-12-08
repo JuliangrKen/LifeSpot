84ef1c1b77ac372a6195728bc48e773aaa153dee
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2022-12-08 18:49:15 +0300

    30.1.12

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,27 +1,42 @@
-﻿let session = {}
-
-function checkAge() {
-    session.userAge = prompt("Пожалуйста, введите ваш возраст")
-
-    if (session.userAge >= 18) {
-        alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + new Date().toLocaleString());
-    } else {
+﻿let checker = function (newVisit) {
+    if (window.sessionStorage.getItem("userAge") >= 18) {
+        // Добавим проверку на первое посещение, чтобы не показывать приветствие
+        // лишний раз
+        if (newVisit) {
+            alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + new Date().toLocaleString());
+        }
+    }
+    else {
         alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
         window.location.href = "http://www.google.com"
     }
-
-    return session.userAge
 }
 
-function handleSession() {
-    session.startDate = new Date().toLocaleString()
-    session.userAgent = window.navigator.userAgent
-}
-
-let sessionLog = function logSession() {
-    for (let key in session) {
-        console.log(session[key])
+function handleSession(logger, checker) {
+    if (window.sessionStorage.getItem("startDate") == null) {
+        window.sessionStorage.setItem("startDate", new Date().toLocaleString())
     }
+
+    if (window.sessionStorage.getItem("userAgent") == null) {
+        window.sessionStorage.setItem("userAgent", window.navigator.userAgent)
+    }
+
+    if (window.sessionStorage.getItem("userAge") == null) {
+        let input = prompt("Пожалуйста, введите ваш возраст?");
+        window.sessionStorage.setItem("userAge", input)
+
+        checker(true)
+    } else {
+        checker(false)
+    }
+
+    logger()
+}
+
+let logger = function () {
+    console.log('Начало сессии: ' + window.sessionStorage.getItem("startDate"))
+    console.log('Даныне клиента: ' + window.sessionStorage.getItem("userAgent"))
+    console.log('Возраст пользователя: ' + window.sessionStorage.getItem("userAge"))
 }
 
 function printSession(session) {
